8731b764af05ba5dae718690789ac6ad4cc0a6e3
ayushpant <ayushp.9645@gmail.com>
2024-01-03 12:49:00 -0500

    added test cases and added deck class

---
+++ AMSGeekChallenge/Card.cs
@@ -0,0 +1,95 @@
+﻿using System;
+namespace AMSGeekChallenge
+{
+    using System;
+    using System.Collections.Generic;
+
+    public enum Suit
+    {
+        Hearts, Diamonds, Clubs, Spades
+    }
+
+    public enum Rank
+    {
+        Ace = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King
+    }
+
+    public class Card
+    {
+        public Suit Suit { get; private set; }
+        public Rank Rank { get; private set; }
+
+        public Card(Suit suit, Rank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rank} of {Suit}";
+        }
+    }
+
+    public class Deck
+    {
+        private List<Card> cards;
+        private Random random;
+
+        public Deck()
+        {
+            random = new Random();
+            cards = new List<Card>();
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    cards.Add(new Card(suit, rank));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            for (int n = cards.Count - 1; n > 0; --n)
+            {
+                int k = random.Next(n + 1);
+                Card temp = cards[n];
+                cards[n] = cards[k];
+                cards[k] = temp;
+            }
+        }
+
+        public Card DealOneCard()
+        {
+            if (cards.Count == 0)
+            {
+                throw new Exception("No cards to deal");
+                
+            }
+
+            Card cardToDeal = cards[0];
+            cards.RemoveAt(0);
+            return cardToDeal;
+        }
+
+        public int GetCardsCount()
+        {
+            return cards.Count;
+        }
+        public List<Card> GetCards()
+        {
+            return new List<Card>(cards);
+        }
+
+        public static void main(String[] args)
+        {
+            Deck deck = new Deck();
+          
+
+        }
+       
+    }
+
+}
+
---
+++ DeckTests/DeckTests.cs
@@ -0,0 +1,59 @@
+﻿
+namespace DeckTests
+{
+
+    using NUnit.Framework;
+    using System.Linq;
+
+
+    [TestFixture]
+    public class DeckTests
+    {
+        private Deck deck;
+
+        [SetUp]
+        public void Setup()
+        {
+            deck = new Deck();
+        }
+
+        [Test]
+        public void Deck_InitializesWith52Cards()
+        {
+            int expectedCardCount = 52;
+            int actualCardCount = deck.GetCardsCount(); // You need to implement GetCardsCount in Deck class
+
+            Assert.AreEqual(expectedCardCount, actualCardCount);
+        }
+
+        [Test]
+        public void Shuffle_RandomizesDeck()
+        {
+            var originalOrder = deck.GetCards().ToList(); // Implement GetCards to return a copy of the cards list
+            deck.Shuffle();
+            var shuffledOrder = deck.GetCards().ToList();
+
+            Assert.IsFalse(originalOrder.SequenceEqual(shuffledOrder));
+        }
+
+        [Test]
+        public void DealOneCard_ReturnsCard()
+        {
+            var card = deck.DealOneCard();
+            Assert.IsNotNull(card);
+            Assert.IsInstanceOf<Card>(card);
+        }
+
+        [Test]
+        public void DealOneCard_RemovesCardFromDeck()
+        {
+            int initialCount = deck.GetCardsCount();
+            deck.DealOneCard();
+            int finalCount = deck.GetCardsCount();
+
+            Assert.AreEqual(initialCount - 1, finalCount);
+
+        }
+
+    }
+}
